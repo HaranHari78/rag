b5ad74c10307321443debcfa049c42bd6f40f9dd
HaranHari78 <hvetri7@gmail.com>
2025-06-16 15:37:20 +0530

    Update cofig.py

--- cofig.py
+++ cofig.py
@@ -7,3 +7,30 @@ api_version = 2024-02-01
 model_gpt4o = OMH-EUS2-GPT4O-1
 
 https://chatgpt.com/share/684fe21e-4d20-8007-9508-17a1cc08f1e4
+
+endpoint = "https://omh-eus-2-test-01.openai.azure.com/"
+
+model_name = "text-embedding-3-large"
+
+deployment = "text-embedding-3-large-EUS-2-01"
+ 
+8804263e0e884a73b493a58f22505cc6
+ 
+endpoint = "https://omh-eus-2-test-01.openai.azure.com/"
+
+model_name = "text-embedding-3-large"
+
+deployment = "text-embedding-3-large-EUS-2-01"
+ 
+api_version = "2024-02-01"
+ 
+client = AzureOpenAI(
+
+    api_version="2024-12-01-preview",
+
+    endpoint=endpoint,
+
+    credential=AzureKeyCredential("<API_KEY>")
+
+)
+ 
