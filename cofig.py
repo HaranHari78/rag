457ea2ce8cf14fdc407fb21ee46c85cf022ecfaf
HaranHari78 <hvetri7@gmail.com>
2025-06-16 16:24:17 +0530

    Update cofig.py

--- cofig.py
+++ cofig.py
@@ -6,6 +6,9 @@ api_version = 2024-02-01
 [gpt_models]
 model_gpt4o = OMH-EUS2-GPT4O-1
 
+[embedding_models]
+text_embedding_3_large = text-embedding-3-large-EUS-2-01
+
 https://chatgpt.com/share/684fe21e-4d20-8007-9508-17a1cc08f1e4
 
 endpoint = "https://omh-eus-2-test-01.openai.azure.com/"
